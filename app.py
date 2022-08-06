3652ba9ebdee959790fa37f8ebda5e3fb7e196bb
Dmytro Znak <DmytroZn>
2022-08-06 21:32:00 +0300

    update pages

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import dash_bootstrap_components as dbc
 from dash import Input, Output, State, html
 import dash
+from flask import Flask, session
 from dash import html, dcc
 
 from templates import *
 
+server = Flask(__name__)
+server.config['SECRET_KEY'] = 'super secret key'
+
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(server=server, external_stylesheets=[dbc.themes.BOOTSTRAP])
 # app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
@@ -24,7 +29,113 @@ def dec(func):
 # ____________________________________________________________________________________________________
 
 
+
 app.layout = main
+
+
+
+# @app.callback(
+#     [
+#      # Output("button-sing-in2", "children"),
+#      # Output("button-sing-up2", "children"),
+#      Output('content2', 'children'),
+#      Output('button-sidebar', 'children'),
+#      ],
+#     [Input("start-page", "children"),
+#      # Input('button-sing-in2', 'n_clicks'),
+#      # Input('button-sing-up2', 'n_clicks'),
+#      Input('button-singup-page', 'n_clicks'),
+#      Input('button-singin-page', 'n_clicks')
+#      ]
+# )
+# def start_page(a, b=0, c=0):
+#     print('sf d d d d d ')
+#     if not session.get('token'):
+#         ctx = dash.callback_context
+#         button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+#         if button_id == 'button-singup-page':
+#             return sing_in, ''
+#         elif button_id == 'button-singin-page':
+#             return sing_up, ''
+#         else:
+#             print(button_id)
+#             return sing_in, ''
+#     else:
+#         return '', button_sidebar\
+
+@app.callback(
+    [
+     Output('button-sing', 'value'),
+     Output('button-sing', 'children'),
+     Output('content', 'children'),
+     Output('button-sidebar', 'children'),
+     ],
+    [
+        Input('button-sing', 'value'),
+        Input('button-sing', 'n_clicks'),
+
+     Input('button-item-1', 'n_clicks'),
+     Input('button-item-2', 'n_clicks'),
+    # Input('button-sing-in', 'n_clicks'),
+
+    ]
+)
+def start_page(value, b, c, d):
+    print('sf d d d d d ', value, session.get('token'))
+    if not session.get('token'):
+        if value == 'sing_in':
+            return 'sing_up', 'sing up', sing_in, ''
+        elif value == 'sing_up':
+            return 'sing_in', 'sing in', sing_up, ''
+        else:
+            return 'sing_up', 'sing up', sing_in, ''
+    elif session.get('token'):
+
+        ctx = dash.callback_context
+        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+        if button_id == "button-item-1":
+            return 'sing_out', 'sing out', [spending_money, add_spending], button_sidebar
+        elif button_id == "button-item-2":
+            return 'sing_out', 'sing out', 'TEST BUTT 2', button_sidebar
+        # return ([spending_money, add_spending],)
+        if value == 'sing_out':
+            del session['token']
+            return 'sing_up', 'sing up', sing_in, ''
+
+        return 'sing_out', 'sing out', '', button_sidebar
+
+
+# @app.callback(
+#     [Output('button-sing', 'value'),
+#     Output('button-sing', 'children'),
+#      Output('content2', 'children'),
+#      Output('button-sidebar', 'children'),
+#      ],
+#     Input('button-sing', 'value'),
+# )
+# def sign_out(a):
+#     del session['token']
+#     return 'sing_up', 'sing up', sing_in, ''
+
+
+@app.callback(
+    Output('page-content', 'children'),
+    Input('button-sing-in', 'n_clicks'),
+    [State('input-username-sing-in', 'value'),
+     State('input-password-sing-in', 'value')]
+
+)
+def sign_in(a, b, c):
+    session['token'] = b
+    return ''
+
+@app.callback(
+    Output('content3', 'children'),
+    Input('button-singup-page', 'n_clicks')
+)
+def tt():
+    print('test')
+    return
 
 
 @app.callback(
@@ -41,28 +152,6 @@ def toggle_offcanvas1(n1, n2, is_open):
     if dropdown_id == "button-item-1":
         return False
     return True
-
-
-# delete
-@app.callback(Output('main-table', 'data'),
-              [Input('button-item-2', 'n_clicks'), Input('button-item-3', 'n_clicks')])
-@dec
-def update_graphs(n2, n3):
-    ctx = dash.callback_context
-    dropdown_id = ctx.triggered[0]['prop_id'].split('.')[0]
-
-    d2 = [{"name": "sdf", "id": "sdfsf"},
-          {"name": "sdf", "id": "sdfsf"},
-          {"name": "sdf", "id": "sdfsf"},
-          {"name": "sdf", "id": "sdfsf"}, ]
-
-    d3 = [{"name": "sdf", "id": "sdfsf"}, ]
-
-    if dropdown_id == "button-item-2":
-        return d2
-    elif dropdown_id == "button-item-3":
-        return d3
-    return [{"name": "", "id": ""}, ]
 
 
 @app.callback(
@@ -118,22 +207,23 @@ def adding_spending(a, b, c, d):
     return ('sd',)
 
 
-@app.callback(
-    [Output('content', 'children')],
-    [Input('button-item-1', 'n_clicks'),
-     Input('button-item-2', 'n_clicks')
-     ],
-)
-@dec
-def change_page(_, __):
-    ctx = dash.callback_context
-    button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+# @app.callback(
+#     [Output('content', 'children')],
+#     [Input('button-item-1', 'n_clicks'),
+#      Input('button-item-2', 'n_clicks')
+#      ],
+# )
+# @dec
+# def change_page(_, __):
+#     ctx = dash.callback_context
+#     button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+#     if button_id == "button-item-1":
+#         return ([spending_money, add_spending],)
+#     elif button_id == "button-item-2":
+#         return ('TEST BUTT 2',)
+#     # return ([spending_money, add_spending],)
+#     return ('',)
 
-    if button_id == "button-item-1":
-        return ([spending_money, add_spending],)
-    elif button_id == "button-item-2":
-        return ('TEST BUTT 2',)
-    return ('sdf',)
 
 
 # ____________________________________________________________________________________________________
--- templates.py
+++ templates.py
@@ -7,6 +7,8 @@ PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
 ###########################################################################
 
 content = html.Div(id='content')
+content2 = html.Div(id='content2')
+content3 = html.Div(id='content3')
 
 
 sidebar = html.Div(
@@ -28,18 +30,25 @@ sidebar = html.Div(
     ]
 )
 
+button_sidebar = dbc.Button("Menu", id="open-offcanvas", n_clicks=0)
 
-navbar = dbc.Navbar(
+button_singup = dbc.Button('Singup', id='button-singup-page', outline=True, name='name dd', value='val', n_clicks=0, style={'color': 'yellow'})
+button_singin = dbc.Button('Singin', id='button-singin-page', active=False, n_clicks=0, style={'color': 'yellow'})
+
+navbar = dbc.Navbar([
     dbc.Container(
         [
             # dbc.NavbarToggler(id="open-offcanvas", n_clicks=0),
-            dbc.Button("Menu", id="open-offcanvas", n_clicks=0),
+            html.Div(id='button-sidebar'),
+            # dbc.Button("Menu", id="open-offcanvas", n_clicks=0),
             html.A(
                 # Use row and col to control vertical alignment of logo / brand
                 dbc.Row(
                     [
                         dbc.Col(html.Img(src=PLOTLY_LOGO, height="30px")),
                         dbc.Col(dbc.NavbarBrand("Navbar", className="ms-2")),
+                        dbc.Col(dbc.Button(id='button-sing', value='test_value', name='test_name',
+                                           style={'color': 'yellow', 'width':'90px'})),
                     ],
                     align="flex-start",
                     className="g-0",
@@ -49,14 +58,11 @@ navbar = dbc.Navbar(
             ),
         ]
     ),
+sidebar],
     color="dark",
     dark=True,
 )
 
-
-table = dbc.Container([
-    dash_table.DataTable(id='main-table')
-])
 
 spending_money = html.Center(html.Div(
     [
@@ -81,6 +87,7 @@ spending_money = html.Center(html.Div(
 
     ], style={'padding-right': '1em'}
 ))
+
 
 add_spending = html.Center(html.Div(
     [
@@ -130,12 +137,12 @@ sing_in = html.Center(html.Div(
         html.Br(),
         dbc.Row(
             dbc.Col(
-                dbc.Button("Singin", id='button-sing-in', n_clicks=0, color="primary"),
+                dbc.Button("Singin", id='button-sing-in', n_clicks=0, href='/sd', color="primary"),
             )
         ),
         html.Br(),
 
-    ]
+    ], style={'padding-right': '1em'}
 ))
 
 
@@ -185,8 +192,11 @@ sing_up = html.Center(html.Div(
         ),
         html.Br(),
 
-    ]
+    ], style={'padding-right': '1em'}
 ))
 
 
-main = html.Div([navbar, sidebar, content, sing_up])
+
+
+# main = html.Div([navbar, sidebar, content, sing_up])
+main = html.Div([navbar, content, content2])
