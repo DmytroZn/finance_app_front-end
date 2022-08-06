73c158b3c746b6fef1558f5f77363e4fe6fdd7b0
Dmytro Znak <DmytroZn>
2022-08-06 13:53:54 +0300

    update UI

--- templates.py
+++ templates.py
@@ -33,7 +33,7 @@ navbar = dbc.Navbar(
     dbc.Container(
         [
             # dbc.NavbarToggler(id="open-offcanvas", n_clicks=0),
-            dbc.Button("Open Offcanvas1", id="open-offcanvas", n_clicks=0),
+            dbc.Button("Menu", id="open-offcanvas", n_clicks=0),
             html.A(
                 # Use row and col to control vertical alignment of logo / brand
                 dbc.Row(
@@ -59,41 +59,106 @@ table = dbc.Container([
 ])
 
 
-spending_money = html.Center([
-    html.Br(),
-    html.Table([
-        html.Tbody([
-            html.Tr([
-                html.P('You have spent money already: '), html.Td(html.P(children="9 000", id='spent-money')),
-            ]),
-            html.Tr([
-                html.P('You have money for spending: '), html.Td(html.P("6 000", id='have-money')),
-            ]),
-            html.Tr([
-                html.P("You can spend today: "), html.Td(html.P("676,98", id='can-spend'))
-            ])
-        ])
-    ])
-])
+# spending_money = html.Center([
+#     html.Br(),
+#     html.Table([
+#         html.Tbody([
+#             html.Tr([
+#                 html.P('You have spent money already: '), html.Td(html.P(children="9 000", id='spent-money')),
+#             ]),
+#             html.Tr([
+#                 html.P('You have money for spending: '), html.Td(html.P("6 000", id='have-money')),
+#             ]),
+#             html.Tr([
+#                 html.P("You can spend today: "), html.Td(html.P("676,98", id='can-spend'))
+#             ])
+#         ])
+#     ])
+# ])
 
-
-
-add_spending = html.Div([
-    html.Center([
-        html.Table([
-            html.Tbody([
-                html.Tr([
-                    (dbc.Input(id="input-money", value='sdf', placeholder="", type="number", min=0, max=15_000_000, style={'width': '150px'})),
-                    html.Td(dcc.Dropdown(id='dropdown-categories', style={'width': '155px'})),
-                ]),
-            ]),
-        ]),
+spending_money = html.Center(html.Div(
+    [
         html.Br(),
-        dbc.Container(dbc.Textarea(id="textarea-comment", rows=2, style={'width': '300px'})),
-    ]),
-    html.Center(dbc.Button("Submit", id='button-add', n_clicks=0, color="primary"), style={"display": "right"})
+        dbc.Row(
+            [
+                dbc.Col([html.P('You have spent money already: '), html.P(children="9 000", id='spent-money')] ),
+                # dbc.Col([html.P(children="9 000", id='spent-money')] ),
+            ]
+        ),
 
-])
+        dbc.Row(
+            [
+                dbc.Col([html.P('You have money for spending: '), html.P("6 000", id='have-money')] ),
+                # dbc.Col([html.P("6 000", id='have-money')] ),
+            ]
+        ),
+
+        dbc.Row(
+            [
+                dbc.Col([html.P('You can spend today: '), html.P("676,98", id='can-spend')] ),
+                # dbc.Col([html.P("676,98", id='can-spend')] ),
+            ]
+        ),
+
+    ], style={'padding-right': '1em'}
+))
+
+
+
+# add_spending = html.Div([
+#     html.Center([
+#         html.Table([
+#             html.Tbody([
+#                 html.Tr([
+#                     (dbc.Input(id="input-money", value='sdf', placeholder="", type="number", min=0, max=15_000_000, style={'width': '150px'})),
+#                     html.Td(dcc.Dropdown(id='dropdown-categories', style={'width': '155px'})),
+#                 ]),
+#             ]),
+#         ]),
+#         html.Br(),
+#         dbc.Container(dbc.Textarea(id="textarea-comment", rows=2, style={'width': '300px'})),
+#     ]),
+#     html.Center(dbc.Button("Submit", id='button-add', n_clicks=0, color="primary"), style={"display": "right"})
+#
+# ])
+
+add_spending = html.Center(html.Div(
+    [
+        dbc.Row(
+            html.Center(
+                dbc.Col(
+                    dbc.Input(id="input-money", value='sdf', placeholder="", type="number", min=0, max=15_000_000,
+                              style={'width': '150px'}),
+                ),
+            )
+        ),
+        html.Br(),
+        dbc.Row(
+            html.Center(
+                dbc.Col(
+                    dcc.Dropdown(id='dropdown-categories', style={'width': '155px', 'text-align': 'center'}),
+                ),
+            )
+        ),
+        html.Br(),
+        dbc.Row(
+            html.Center(
+                dbc.Col(
+                    dbc.Textarea(id="textarea-comment", rows=2, style={'width': '300px'}),
+                )
+            )
+        ),
+        html.Br(),
+        dbc.Row(
+            html.Center(
+                dbc.Col(
+                    dbc.Button("Submit", id='button-add', n_clicks=0, color="primary"),
+                )
+            )
+        ),
+        html.Br(),
+    ], style={'padding-right': '1em'}
+))
 
 
 
